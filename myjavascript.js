d11635e4b78cfc821495ce5c34dcbfa893f2fb34
Ayat Amin <ayat.jafar.amin@gmail.com>
2023-04-02 16:26:44 -0400

    submit

--- myjavascript.js
+++ myjavascript.js
@@ -3,6 +3,8 @@
   const emailInput = document.querySelector('#email');
   const checkboxInput = document.querySelector('#accurate');
   const errorElement = document.querySelector('#error');
+  const confirmation = document.querySelector('#confirmation');
+  const confirmationMessage = document.getElementById('confirmationMessage');
 
   form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -22,11 +24,17 @@
       messages.push('You must confirm that all information is accurate');
     }
 
+    const name = document.getElementById('name').value;
+    confirmationMessage.textContent = `Thank you, ${name}!`;
+
+
     if (messages.length > 0) {
       errorElement.innerText = messages.join(', ');
     }else {
-      form.style.display = 'none';
-      confirmationElement.style.display = 'block';
+      form.classList.add('hidden');
+      confirmation.classList.remove('hidden');
+      // form.style.display = 'none';
+      // confirmationElement.style.display = 'block';
     }
   });
 
